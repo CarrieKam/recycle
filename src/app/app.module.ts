e2cdb615b29999c4796ec60b8118e941915c5c1a
Carrie Kam <satoshiminamisama@gmail.com>
2022-08-28 08:33:15 -0400

    created an options component, search bar doesn't work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { OptionsComponent } from './options/options.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OptionsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/options/options.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
+@Component({
+  selector: 'options',
+  templateUrl: './options.component.html',
+  styleUrls: ['./options.component.css'],
+})
+export class OptionsComponent{
+
+}
